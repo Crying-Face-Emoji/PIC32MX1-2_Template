a92f81cc11b640ce06fea0223612dae37b0d4f9e
Crying-Face-Emoji <39195157+Crying-Face-Emoji@users.noreply.github.com>
2018-05-29 00:01:50 +0100

    fixed speed selection on i2c lib

--- template/lib/I2C_pic32mx.cpp
+++ template/lib/I2C_pic32mx.cpp
@@ -7,59 +7,38 @@
 
 #include <proc/p32mx150f128b.h>
 #include "I2C_pic32mx.h"
-//#include "UART_pic32mx.h"
+#include "UART_pic32mx.h"
 
-#define F_SCL_NORMAL 100000UL
-#define F_SCL_FAST 400000UL
-
-/*
-I2Cx_Init
-I2Cx_Init_Advanced
-I2Cx_Start
-I2Cx_Restart
-I2Cx_Is_Idle
-I2Cx_Read
-I2Cx_Write
-I2Cx_Stop
- */
-
-void I2C_pic32mx::Init(bool fastmode)
+void I2C_pic32mx::Init(uint32_t speed)
 {
 	ANSELBCLR = 0x00000300;
 	LATBCLR = 0x00000300;
 	TRISBCLR = 0x00000300;
-//	CNPUBSET = 0x00000300;
 	
-//	I2C1BRG = (((1/(2*(fastmode ? F_SCL_NORMAL : F_SCL_FAST)))*GetPeripheralClock()) - 2);
-	I2C1BRG = 8;
+	I2C1BRG = (((1.f/(2.f*(float)speed)) - 0.000000104f ) * (float)GetPeripheralClock()) - 2.f;
 	
-//	I2C1CONbits.DISSLW = !(fastmode);
-	I2C1CONbits.DISSLW = 0;
+	I2C1CONbits.DISSLW = !(speed == F_SCL_FAST);
 	I2C1CONbits.ON = 1;
 }
 
 uint8_t I2C_pic32mx::Transmit(uint8_t address, uint8_t* src, uint16_t count, bool stop)
 {
-	static bool stoplast = 1;
 	
-//	IFS1bits.I2C1MIF = 0;
-	if(stoplast)	/* if last state was a stop bit*/
+	if(I2C1STATbits.P)	/* if last state was a stop bit*/
 	{
-		I2C1ACONbits.SEN = 1;	/* else restart condition */
+		I2C1ACONbits.SEN = 1;	/* Start condition */
 		while(I2C1ACONbits.SEN);
-//		IFS1bits.I2C1MIF = 0;
 	}
 	else
 	{
-		I2C1ACONbits.RSEN = 1;	/* Start condition */
+		I2C1ACONbits.RSEN = 1;	/* else restart condition */
 		while(I2C1ACONbits.RSEN);
-//		IFS1bits.I2C1MIF = 0;
 	}
+	if(I2C1STATbits.BCL == 1)
+		return 0;
 	
-//	UART1.Printf("transmit\n");
 	I2C1TRN = address;
 	while(I2C1STATbits.TRSTAT);	/* Master transmit is in progress (8 bits + ACK) */
-//	IFS1bits.I2C1MIF = 0;
 	if(I2C1STATbits.ACKSTAT)		/* 1 = Acknowledge was not received */
 	{
 		I2C1CONbits.PEN = 1;
@@ -75,12 +54,10 @@ uint8_t I2C_pic32mx::Transmit(uint8_t address, uint8_t* src, uint16_t count, boo
 			{
 				I2C1TRN = *(src++);
 				while(I2C1STATbits.TRSTAT);	/* Master transmit is in progress (8 bits + ACK) */
-//				IFS1bits.I2C1MIF = 0;
 				if(I2C1STATbits.ACKSTAT)		/* 1 = Acknowledge was not received */
 				{
 					I2C1CONbits.PEN = 1;
 					while(I2C1CONbits.PEN);
-//					IFS1bits.I2C1MIF = 0;
 					return 0;
 				}
 			}
@@ -88,24 +65,19 @@ uint8_t I2C_pic32mx::Transmit(uint8_t address, uint8_t* src, uint16_t count, boo
 			{
 				I2C1CONbits.ACKDT =  !(count > 1);
 				
-//				UART1.Printf("read\n");
 				I2C1CONbits.RCEN = 1;
 				while(I2C1CONbits.RCEN);
-//				IFS1bits.I2C1MIF = 0;
 
 				I2C1CONbits.ACKEN = 1;
 				while(I2C1CONbits.ACKEN);
-//				IFS1bits.I2C1MIF = 0;
 				*(src++) = I2C1RCV;
 			}
 		} 
 		while(--count);
 	} 
 
-	stoplast = stop;
 	I2C1CONbits.PEN = stop;
 	while(I2C1CONbits.PEN);
-//	IFS1bits.I2C1MIF = 0;
 	
 	return 1;
 }
--- template/lib/I2C_pic32mx.h
+++ template/lib/I2C_pic32mx.h
@@ -12,25 +12,25 @@
 #include <sys/attribs.h>
 #include "../HardwareConfig.h"
 
-#define I2C_PRESCALER 1
+#define F_SCL_NORMAL	100000UL
+#define F_SCL_FAST		400000UL
 
-#define I2C_NORMAL 0
-#define I2C_FAST 1
-
-#define I2C_RESTART 0
-#define I2C_STOP 1
+#define I2C_RESTART	0
+#define I2C_STOP		1
 
 class I2C_pic32mx
 {
 	public:
 	inline I2C_pic32mx() {}
 	
-	void Init(bool fastmode);
+	void Init(uint32_t speed);
 
 	uint8_t Transmit(uint8_t address, uint8_t* src, uint16_t count, bool stop);
 
 	inline uint8_t Write(uint8_t address, uint8_t* src, uint16_t count, bool stop)
 	{
+		if(((address < 0x08) || (address > 0x77)) && ((address != 0x00)))
+			return 0;
 		return Transmit((address << 1) & ~0x01, src, count, stop);
 	}
 	
@@ -41,6 +41,9 @@ class I2C_pic32mx
 
 	inline uint8_t Read(uint8_t address, uint8_t* dst, uint16_t count, bool stop)
 	{
+		if(((address < 0x08) || (address > 0x77)) && ((address != 0x00)))
+			return 0;
+		
 		return Transmit((address << 1) | 0x01, dst, count, stop);
 	}
 	
@@ -55,3 +58,17 @@ class I2C_pic32mx
 } ;
 
 #endif	/* I2C_PIC32MX_H */
+
+//UART1.Printf("\nprogram init\n");
+//		
+//	UART1.Printf("performing I2C check\n");	
+//	
+//	for(uint8_t a = 0x01; a < 0x7F; a++)
+//	{
+//		if(I2C.Write(a, nullptr, 0, 1))
+//		{
+//			UART1.Printf("  I2C Address 0x%02X ACKed!\n", a);
+//		}
+//	}
+//	
+//	UART1.Printf("I2C check done\n");
